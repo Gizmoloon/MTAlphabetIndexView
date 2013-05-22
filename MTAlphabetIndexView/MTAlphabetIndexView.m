4fd7331d6f790d108bd877f69f90ee637f2b574f
Glenn Forbes <me@gizmoloon.com>
2013-05-22 13:48:04 +0200

    Updated to work with the Retina display, support a number section and made the Hide selector public.

--- MTAlphabetIndexView/MTAlphabetIndexView.m
+++ MTAlphabetIndexView/MTAlphabetIndexView.m
@@ -48,8 +48,12 @@
 {
     for (int i = 0; i < length / 4; i ++)
     {
+        
+        float scale = [self contentScaleFactor];
+        CGRect bounds = CGRectMake(15.0f, 8.0f + i * 65.0f, 290.0f * scale, 55.0f * scale);
         CALayer *layer = [CALayer layer];
-        layer.frame = CGRectMake(15.0f, 8.0f + i * 65.0f, 290.0f, 55.0f);
+        layer.frame = bounds;
+        layer.contentsScale = [[UIScreen mainScreen] scale];
         
         for (int j = 0; j < 4; j ++)
         {
@@ -58,17 +62,27 @@
             CALayer *alphabetLayer = [CALayer layer];
             alphabetLayer.frame = CGRectMake(j * 75.0f, 0.0f, 65.0f, 55.0f);
             alphabetLayer.backgroundColor = [UIColor redColor].CGColor;
-            [layer addSublayer:alphabetLayer];
+            alphabetLayer.contentsScale = [[UIScreen mainScreen] scale];
+            if (![layer.sublayers containsObject:alphabetLayer])
+            {
+                [layer addSublayer:alphabetLayer];
+            }
             
             CATextLayer *textLayer = [CATextLayer layer];
-            textLayer.frame = CGRectMake(10.0f, 25.0f, 30.0f, 30.0f);
+            textLayer.frame = CGRectMake(7.0f, 22.0f, 30.0f, 30.0f);
             textLayer.string = [self alphabetAtIndex:index];
-            textLayer.font = @"Helvetica-Bold";
-            textLayer.fontSize = 24.0f;
-            [alphabetLayer addSublayer:textLayer];
+            textLayer.font = @"SourceSansPro-Semibold";
+            textLayer.fontSize = 25.0f;
+            textLayer.contentsScale = [[UIScreen mainScreen] scale];
+            if (![alphabetLayer.sublayers containsObject:textLayer])
+            {
+                [alphabetLayer addSublayer:textLayer];
+            }
+            
             
             _alphabetLayers[index] = alphabetLayer;
         }
+        
         
         [self.layer addSublayer:layer];
     }
@@ -82,18 +96,36 @@
         if (_status[i])
         {
             CALayer *layer = _alphabetLayers[i];
-            layer.backgroundColor = [UIColor colorWithRed:255.0/255 green:85.0f/255 blue:119.0/255 alpha:1.0f].CGColor;
+            UIColor *def = [NSKeyedUnarchiver unarchiveObjectWithData:[[NSUserDefaults standardUserDefaults] objectForKey:@"accentColor"]];
+            layer.backgroundColor = def.CGColor;
+            layer.contentsScale = [[UIScreen mainScreen] scale];
             
             CATextLayer *textLayer = layer.sublayers.lastObject;
             textLayer.foregroundColor = [UIColor whiteColor].CGColor;
+            textLayer.contentsScale = [[UIScreen mainScreen] scale];
         }
-        else 
+        else if (i == 0)
         {
             CALayer *layer = _alphabetLayers[i];
-            layer.backgroundColor = [UIColor colorWithWhite:66./255 alpha:1.].CGColor;
+            UIColor *def = [NSKeyedUnarchiver unarchiveObjectWithData:[[NSUserDefaults standardUserDefaults] objectForKey:@"accentColor"]];
+            layer.backgroundColor = def.CGColor;
+            layer.contentsScale = [[UIScreen mainScreen] scale];
             
             CATextLayer *textLayer = layer.sublayers.lastObject;
-            textLayer.foregroundColor = [UIColor colorWithWhite:204./255 alpha:1.].CGColor;
+            textLayer.foregroundColor = [UIColor whiteColor].CGColor;
+            textLayer.contentsScale = [[UIScreen mainScreen] scale];
+        }
+        else
+        {
+            CALayer *layer = _alphabetLayers[i];
+            UIColor *current = [NSKeyedUnarchiver unarchiveObjectWithData:[[NSUserDefaults standardUserDefaults] objectForKey:@"accentColor"]];
+            UIColor *augmented = [current colorWithAlphaComponent:0.5];
+            layer.backgroundColor = augmented.CGColor;
+            layer.contentsScale = [[UIScreen mainScreen] scale];
+    
+            CATextLayer *textLayer = layer.sublayers.lastObject;
+            textLayer.foregroundColor = augmented.CGColor;
+            textLayer.contentsScale = [[UIScreen mainScreen] scale];
         }
     }
 }
@@ -112,17 +144,18 @@
 
 - (NSString *)alphabetAtIndex:(NSInteger)index
 {
-    if (index == 0)
+    if (index > 0 && index < 27)
+    {
+        return [[NSString stringWithFormat:@"%c", 'A' + index - 1] lowercaseString];
+    }
+    else if (index == 0)
     {
         return @"#";
     }
-    
-    if (index > 0 && index < 27)
+    else
     {
-        return [NSString stringWithFormat:@"%c", 'A' + index - 1];
+        return nil;
     }
-    
-    return @"";
 }
 
 
@@ -149,15 +182,26 @@
     [self reloadData];
     
     CATransform3D transform = CATransform3DIdentity;
-    float zDistanse = 500.0f;
-    transform.m34 = 1.0f / zDistanse;
+    float zDistance = 500.0f;
+    transform.m34 = 1.0f / zDistance;
     
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform"];
     animation.fromValue = [NSValue valueWithCATransform3D:CATransform3DRotate(transform,-0.8f ,1.0f,0.0f,0.0f)];
     animation.toValue = [NSValue valueWithCATransform3D:CATransform3DRotate(transform,0.0f,1.0f,0.0f,0.0f)];
     animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
     [animation setDuration:0.2f];
-
+    button = [[UIButton alloc] initWithFrame:[[self superview] bounds]];
+    [button addTarget:self action:@selector(hide) forControlEvents:UIControlEventTouchUpInside];
+    [[self superview] addSubview:button];
+    for (int gd = 0; gd<[[[self superview] subviews] count]; gd++)
+    {
+        NSArray *a = [[self superview] subviews];
+        UIView *sub = a[gd];
+        if (sub != self)
+        {
+            [sub setAlpha:0];
+        }
+    }
     [CATransaction begin];
     
     for (CALayer *layer in self.layer.sublayers)
@@ -172,25 +216,36 @@
 - (void)hide
 {
     CATransform3D transform = CATransform3DIdentity;
-    float zDistanse = 500.0f;
-    transform.m34 = 1.0f / zDistanse;
+    float zDistance = 500.0f;
+    transform.m34 = 1.0f / zDistance;
     
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform"];
     animation.fromValue = [NSValue valueWithCATransform3D:CATransform3DRotate(transform, 0.0f, 1.0f, 0.0f, 0.0f)];
     animation.toValue = [NSValue valueWithCATransform3D:CATransform3DRotate(transform, 1.5f , 1.0f, 0.0f, 0.0f)];
     animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
     [animation setDuration:0.15f];
-    
+    [[self superview] setAlpha:1];
     [CATransaction begin];
-    
+    [CATransaction setAnimationDuration:0.3f];
     for (CALayer *layer in self.layer.sublayers)
     {
         [layer addAnimation:animation forKey:nil];
+        layer.opacity = 0;
     }
     
+    for (int gd = 0; gd<[[[self superview] subviews] count]; gd++)
+    {
+        NSArray *a = [[self superview] subviews];
+        UIView *sub = a[gd];
+        if (sub != self)
+        {
+           [sub setAlpha:1]; 
+        }
+    }
+    [button removeFromSuperview];
     [CATransaction setCompletionBlock:^{
 
-        [self performSelector:@selector(removeFromSuperview) withObject:nil afterDelay:0.1];
+        [self performSelector:@selector(removeFromSuperview) withObject:nil afterDelay:0.2];
         
     }];
     
@@ -206,6 +261,11 @@
     NSInteger index = [self calIndexWithPoint:touchPoint];
     
     if (index > -1 && _status[index])
+    {
+        [self.delegate alphabetIndexView:self alphabetDidSelect:index];
+        [self hide];
+    }
+    else if (index == 0)
     {
         [self.delegate alphabetIndexView:self alphabetDidSelect:index];
         [self hide];
